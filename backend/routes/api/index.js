4fd141a7bbabf82e885427048ad98ddb668d2f16
edgar <johnnyavila2021@u.northwestern.edu>
2023-05-20 02:06:26 -0400

    add functionality to restore user

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,37 @@
 const router = require('express').Router();
+const { restoreUser, setTokenCookie, requireAuth } = require("../../utils/auth.js");
+const { User } = require('../../db/models');
+router.use(restoreUser);
+
+router.get(
+    '/restore-user',
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'demoEdgar'
+        }
+    });
+
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+
+    setTokenCookie(res, user);
+    return res.json({ user: wuser });
+});
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
+const { setTokenCookie } = require('../utils/auth.js')
+const { User } = require('../db/models')
+const { restoreUser } = require('../utils/auth.js')
 
 router.use('/api', apiRouter);
+router.use(restoreUser);
 
 
-router.get("/api/csrf/restore", (req, res) => {
+
+router.get("/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
     res.status(200).json({
