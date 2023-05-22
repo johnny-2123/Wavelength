db7670a73e349fd533747012eae91e8a3b0d17ba
edgar <johnnyavila2021@u.northwestern.edu>
2023-05-22 04:50:35 -0400

    add signup form react component and redux thunk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import LoginFormPage from "./components/User/LoginFormPage";
+import SignupFormPage from "./components/User/SignUpFormPage";
 import Home from "./components/Home";
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
       <Route path="/">
         <Home />
