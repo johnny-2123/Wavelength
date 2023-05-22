81863bb78ad0131aab8ba531075e005ee4ec8a83
edgar <johnnyavila2021@u.northwestern.edu>
2023-05-22 03:57:39 -0400

    add proxy to frontend/package.json and wrap fetch requests with CSRF protection

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,16 +5,18 @@ const rootReducer = combineReducers({
   // add reducer functions here
 });
 
+
 let enhancer;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require("redux-logger").default;
+  const logger = require('redux-logger').default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
+
 
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
