d5106bebdcba8aebc7dd1f5a2eae812e946281aa
edgar <johnnyavila2021@u.northwestern.edu>
2023-05-25 13:31:06 -0400

    add css styling to home page and login modal

--- frontend/src/components/Home/LoggedInHomePage/index.js
+++ frontend/src/components/Home/LoggedInHomePage/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+
 import "./LoggedInHomePage.css";
 
 const LoggedInUserHomePage = ({ sessionUser }) => {
