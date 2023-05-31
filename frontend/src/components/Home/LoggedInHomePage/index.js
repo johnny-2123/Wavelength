c8a0efa432caad912f89c1e00d0fcd526109c5c8
edgar <johnnyavila2021@u.northwestern.edu>
2023-05-30 20:21:31 -0400

    clean up LoggedInHomePage component

--- frontend/src/components/Home/LoggedInHomePage/index.js
+++ frontend/src/components/Home/LoggedInHomePage/index.js
@@ -11,7 +11,7 @@ import GameInviteRequestComponent from "../../GameInviteRequestComponent";
 import GamePlay from "../../GamePlay";
 import useWebSocket from "./useWebsocket";
 import { useModal } from "../../../context/modal";
-import './LoggedInHomePage.css'
+import './LoggedInHomePage.css';
 
 const LoggedInUserHomePage = ({ sessionUser }) => {
     const dispatch = useDispatch();
@@ -23,14 +23,12 @@ const LoggedInUserHomePage = ({ sessionUser }) => {
     const [acceptedFriendsUserNamesArray, setAcceptedFriendsUserNamesArray] = useState([]);
     const [shouldConnect, setShouldConnect] = useState(false);
 
-    const game = useSelector((state) => state?.games?.currentGame)
-    console.log('game in home component', game)
+    const game = useSelector((state) => state?.games?.currentGame);
+    console.log('game in home component', game);
 
-    const [showGamePlay, setShowGamePlay] = useState(game?.id !== undefined && !game?.gameOver);
+    const [showGamePlay, setShowGamePlay] = useState(false);
 
-    console.log('showGamePlay', showGamePlay)
-
-
+    console.log('showGamePlay', showGamePlay);
 
     const handleFriendStatusChange = (data, statusType) => {
         let username = data.username;
@@ -80,19 +78,17 @@ const LoggedInUserHomePage = ({ sessionUser }) => {
     const { sendMessage } = useWebSocket(generateWebSocketURL(), messageHandlers, shouldConnect);
 
     useEffect(() => {
-        if (game.id !== undefined && !game?.gameOver) {
-            console.log('game not over home component')
+        if (game?.id !== undefined && !game?.gameOver) {
+            console.log('game not over home component');
             setShowGamePlay(true);
             history.push(`/gameplay/${game?.id}`);
         } else {
-            console.log('game is over home component')
+            console.log('game is over home component');
             setShowGamePlay(false);
             history.push('/');
         }
-    }, [game])
+    }, [game, history]);
 
-
-    // Fetch friends when the component mounts
     useEffect(() => {
         dispatch(fetchFriends()).then((fetchedFriends) => {
             const acceptedFriendsUserNames = fetchedFriends
@@ -109,18 +105,20 @@ const LoggedInUserHomePage = ({ sessionUser }) => {
         return () => {
             dispatch(fetchSetCurrentUserOffline());
         };
-    }, [dispatch, sessionUser?.id]);
+    }, [dispatch, sessionUser]);
 
     return (
         <div className="homePageLoggedInMainDiv">
-            {!showGamePlay && <nav className="NavBar">
-                <NavLink to="/direct-message-form" activeClassName="active-link" exact>
-                    Direct Message Form
-                </NavLink>
-                <NavLink to="/friends-list" activeClassName="active-link" exact>
-                    Friends List
-                </NavLink>
-            </nav>}
+            {!showGamePlay && (
+                <nav className="NavBar">
+                    <NavLink to="/direct-message-form" activeClassName="active-link" exact>
+                        Direct Message Form
+                    </NavLink>
+                    <NavLink to="/friends-list" activeClassName="active-link" exact>
+                        Friends List
+                    </NavLink>
+                </nav>
+            )}
             <Switch>
                 <Route path="/gameplay/:gameId">
                     <GamePlay sessionUser={sessionUser} sendMessage={sendMessage} setShowGamePlay={setShowGamePlay} />
@@ -132,7 +130,7 @@ const LoggedInUserHomePage = ({ sessionUser }) => {
                     <FriendsList friends={friends} sessionUser={sessionUser} sendMessage={sendMessage} />
                 </Route>
             </Switch>
-        </div >
+        </div>
     );
 };
 
