ad11d4e584a2eb5e98b418c692a4330ec3f17a8a
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-08 17:57:23 -0400

    refactor pending and accepted friends components to use one component for mapping accepted or pending friends

--- frontend/src/components/Friends/Accepted Friends/Accepted Friend Item/index.js
+++ frontend/src/components/Friends/Accepted Friends/Accepted Friend Item/index.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchAcceptFriendRequest, fetchRejectFriendRequest } from "../../../../store/friends";
 // import "./AcceptedFriends.css";
 
 const AcceptedFriendItem = ({ friend, sessionUser, sendMessage, handleFriendItemClick }) => {
+    const dispatch = useDispatch();
+
+    const [errors, setErrors] = useState([]);
+    const [message, setMessage] = useState("");
+
     const friendUser = friend?.userId === sessionUser?.id ? friend?.ReceivingUser : friend?.RequestingUser;
 
     const friendIconId = friend.status === "accepted" ? "friendIcon" : "pendingFriendIcon";
@@ -18,18 +25,62 @@ const AcceptedFriendItem = ({ friend, sessionUser, sendMessage, handleFriendItem
         });
     };
 
+
+    const handleRejectFriendRequest = async (friendId, status) => {
+        return dispatch(fetchRejectFriendRequest(friendId, status))
+            .then(async (data) => {
+                if (data && data.message) {
+                    setErrors([]);
+                    setMessage(data.message);
+                }
+            })
+            .catch(async (res) => {
+                console.log('res', res);
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+                console.log('data', data.errors);
+            });
+    };
+
+    const handleAcceptFriendRequest = async (friendId, status) => {
+        return dispatch(fetchAcceptFriendRequest(friendId, status))
+            .then(async (data) => {
+                if (data && data.message) {
+                    setErrors([]);
+                    setMessage(data.message);
+                }
+            })
+            .catch(async (res) => {
+                console.log('res', res);
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+                console.log('data', data.errors);
+            });
+    };
+
+
     return (
         <div key={friend.id} className="friendItem">
-            <div className="friendIconDiv">
-                <i class="fa-solid fa-user"></i>
-            </div>
-            <div onClick={() => handleFriendItemClick(friendUser.id)} className="friendInfo">
-                <div className="friendUsername">{friendUser?.username}</div>
-                <div className="friendName">{friendUser?.firstName}</div>
+            <div className="friendItemLeftDiv">
+                <div className="friendIconDiv">
+                    <i class={`fa-solid fa-user ${friendIconId}`}></i>
+                </div>
+                <div onClick={() => handleFriendItemClick(friendUser.id)} className="friendInfo">
+                    <div className="friendUsername">{friendUser?.username}</div>
+                    <div className="friendName">{friendUser?.firstName}</div>
+                </div>
             </div>
             {onlineStatus && <div className="onlineStatus"></div>}
             <div className="friendButtons">
-                <button onClick={handleSendGameInvite}>New Game</button>
+                {friend?.status === 'accepted' && <button onClick={handleSendGameInvite}>New Game</button>}
+                {friend?.status === 'pending' && <>
+                    <button
+                        onClick={() => handleAcceptFriendRequest(friendUser.id, "accepted")}
+                    >Accept</button>
+                    <button
+                        onClick={() => handleRejectFriendRequest(friendUser.id, "rejected")}
+                    >Decline</button>
+                </>}
             </div>
         </div>
     );
--- frontend/src/components/Friends/Pending Friends/index.js
+++ frontend/src/components/Friends/Pending Friends/index.js
@@ -1,77 +1,18 @@
 import React, { useState } from "react";
 import { NavLink, Route, Switch, useParams, useRouteMatch } from "react-router-dom";
 import { useDispatch } from "react-redux";
-
 import { fetchAcceptFriendRequest, fetchRejectFriendRequest } from "../../../store/friends";
+import AcceptedFriendItem from '../Accepted Friends/Accepted Friend Item';
 import "./PendingFriends.css";
 
 
 const PendingFriends = ({ friends, sessionUser }) => {
-    const dispatch = useDispatch();
-
-    const [errors, setErrors] = useState([]);
-    const [message, setMessage] = useState("");
 
     const pendingFriendRequestsReceived = friends?.filter((friend) => friend.status === "pending" && friend.friendId === sessionUser?.id);
 
     const pendingFriendRequestsReceivedMapped = pendingFriendRequestsReceived?.map((friend) => {
-        const friendUser = friend?.userId === sessionUser?.id ? friend?.ReceivingUser : friend?.RequestingUser;
-
-        const friendIconId = friend.status === "accepted" ? "friendIcon" : "pendingFriendIcon";
-
-        return (
-            <div key={friend.id} className="friendItem">
-                <div className="friendIconDiv">
-                    <i id={friendIconId} className="fa-regular fa-user"></i>
-                </div>
-                <div className="friendInfo">
-                    <div className="friendUsername">{friendUser?.username}</div>
-                    <div className="friendName"> {friendUser?.firstName}</div>
-                </div>
-                <div className="friendButtons">
-                    <button
-                        onClick={() => handleAcceptFriendRequest(friendUser.id, "accepted")}
-                    >Accept</button>
-                    <button
-                        onClick={() => handleRejectFriendRequest(friendUser.id, "rejected")}
-                    >Decline</button>
-                </div>
-            </div>
-        );
+        return <AcceptedFriendItem friend={friend} sessionUser={sessionUser} />
     });
-
-    const handleRejectFriendRequest = async (friendId, status) => {
-        return dispatch(fetchRejectFriendRequest(friendId, status))
-            .then(async (data) => {
-                if (data && data.message) {
-                    setErrors([]);
-                    setMessage(data.message);
-                }
-            })
-            .catch(async (res) => {
-                console.log('res', res);
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors);
-                console.log('data', data.errors);
-            });
-    };
-
-    const handleAcceptFriendRequest = async (friendId, status) => {
-        return dispatch(fetchAcceptFriendRequest(friendId, status))
-            .then(async (data) => {
-                if (data && data.message) {
-                    setErrors([]);
-                    setMessage(data.message);
-                }
-            })
-            .catch(async (res) => {
-                console.log('res', res);
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors);
-                console.log('data', data.errors);
-            });
-    };
-
 
     return (
         <div className="friendsContainer">
