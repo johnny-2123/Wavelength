8ce73011ec62d57a6ad3127f27551ac9d4056a33
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-09 19:33:29 -0400

    add top friends component to render friends user has played most games with over the past week

--- frontend/src/components/Home/LoggedInHomePage/LoggedInHomePageRootLanding/Top Friends/index.js
+++ frontend/src/components/Home/LoggedInHomePage/LoggedInHomePageRootLanding/Top Friends/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./TopFriends.css";
 
-const TopFriends = () => {
+const TopFriends = ({ topFriends }) => {
+    console.log('topFriends in top friends component', topFriends)
 
+    const topFriendsMapped = topFriends?.map((friend) => {
+
+        return (
+            <div className="topFriendsItem">
+                <h3><i class="fa-solid fa-circle-dot"></i> {friend?.username}</h3>
+            </div>
+        )
+    })
 
     return (
         <div className="topFriendsMainDiv">
-            <h1>Top Friends</h1>
+            <div className="topFriendsLeftDiv">
+                <h1>Week's Best Friends <i className="fa-solid fa-users"></i></h1>
+            </div>
+            <div className="topFriendsRightDiv">
+                {topFriendsMapped}
+            </div>
         </div>
     )
 }
