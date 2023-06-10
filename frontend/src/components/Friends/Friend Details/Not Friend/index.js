bdc489ffac59e742cc3f351e4a5de191de218cd7
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-10 07:59:50 -0400

    add README page

--- frontend/src/components/Friends/Friend Details/Not Friend/index.js
+++ frontend/src/components/Friends/Friend Details/Not Friend/index.js
@@ -3,14 +3,12 @@ import './NotFriend.css';
 
 const NotFriend = ({ friend, handleSendFriendRequest }) => {
     return (
-        <div>
-            <div className="friendDetailsNotFriend">
-                <h1>{friend?.username}</h1>
-                <h2>{friend?.firstName}</h2>
-                <button
-                    onClick={() => handleSendFriendRequest(friend?.id, 'pending')}
-                    className="friendDetailsButton">Send Friend Request <i class="fa-solid fa-user-plus"></i></button>
-            </div>
+        <div className="friendDetailsNotFriend">
+            <h1>{friend?.username}</h1>
+            <h2>{friend?.firstName}</h2>
+            <button
+                onClick={() => handleSendFriendRequest(friend?.id, 'pending')}
+                className="friendDetailsButton">Send Friend Request <i class="fa-solid fa-user-plus"></i></button>
         </div>
     )
 }
