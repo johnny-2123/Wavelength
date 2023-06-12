24b201d9bcac27dcf451597749494c507a85f73c
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-12 13:47:21 -0400

    add Levenshtein distance function to accept word match even if word has an edit distance of 2

--- frontend/src/components/GamePlay/useGameStatus.js
+++ frontend/src/components/GamePlay/useGameStatus.js
@@ -3,6 +3,51 @@ import { useDispatch } from "react-redux";
 import { updateGame } from "../../store/game";
 import { fetchCreateRound } from "../../store/rounds";
 
+function areWordsSimilar(word1, word2) {
+    const threshold = 2; // Maximum allowed edit distance
+
+    // Calculate the Levenshtein distance between the words
+    const distance = levenshteinDistance(word1.toLowerCase(), word2.toLowerCase());
+
+    // Compare the distance with the threshold
+    return distance <= threshold;
+}
+
+function levenshteinDistance(word1, word2) {
+    const m = word1.length;
+    const n = word2.length;
+
+    // Create a matrix to store the distance values
+    const matrix = Array(m + 1)
+        .fill(null)
+        .map(() => Array(n + 1).fill(null));
+
+    // Initialize the matrix with base values
+    for (let i = 0; i <= m; i++) {
+        matrix[i][0] = i;
+    }
+
+    for (let j = 0; j <= n; j++) {
+        matrix[0][j] = j;
+    }
+
+    // Calculate the distance using dynamic programming
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            const cost = word1[i - 1] === word2[j - 1] ? 0 : 1;
+            matrix[i][j] = Math.min(
+                matrix[i - 1][j] + 1, // Deletion
+                matrix[i][j - 1] + 1, // Insertion
+                matrix[i - 1][j - 1] + cost // Substitution
+            );
+        }
+    }
+
+    // Return the distance between the words
+    return matrix[m][n];
+}
+
+
 export default function useGameStatus(gameId, roundNumber, sendMessage) {
     const dispatch = useDispatch();
 
@@ -38,7 +83,7 @@ export default function useGameStatus(gameId, roundNumber, sendMessage) {
         };
 
         if (Words.length === 2) {
-            if (Words[0]?.wordText === Words[1]?.wordText) {
+            if (areWordsSimilar(Words[0]?.wordText, Words[1]?.wordText)) {
                 sendGameWonMessage();
             } else {
                 sendRoundResults();
