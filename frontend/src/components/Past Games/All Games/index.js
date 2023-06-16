da68fad5e23b458a1144e907ad780db1a365fb02
edgar <johnnyavila2021@u.northwestern.edu>
2023-06-15 21:20:12 -0400

    update css styling in past game components

--- frontend/src/components/Past Games/All Games/index.js
+++ frontend/src/components/Past Games/All Games/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchGames } from '../../../store/game';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import LastGame from '../../Home/LoggedInHomePage/LoggedInHomePageRootLanding/Last Game';
 import './AllGames.css';
 
@@ -10,12 +12,14 @@ const AllGames = ({ sessionUser }) => {
 
     const [earliestFirst, setEarliestFirst] = useState(true);
     const [latestFirst, setLatestFirst] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
 
     let latestClassName = latestFirst ? "timeQueryButtonClick" : "timeQueryButtonNotClicked";
     let earliestClassName = earliestFirst ? "timeQueryButtonClick" : "timeQueryButtonNotClicked";
 
-    const handleLatestFirst = () => {
+    const handleLatestFirst = (e) => {
+        e.stopPropagation();
         if (!latestFirst) {
             setLatestFirst(true);
             setEarliestFirst(false);
@@ -24,7 +28,8 @@ const AllGames = ({ sessionUser }) => {
         }
     };
 
-    const handleEarliestFirst = () => {
+    const handleEarliestFirst = (e) => {
+        e.stopPropagation();
         if (!earliestFirst) {
             setEarliestFirst(true);
             setLatestFirst(false);
@@ -63,19 +68,39 @@ const AllGames = ({ sessionUser }) => {
             });
     }, [dispatch]);
 
+    const handleDropdownOpen = () => {
+        setIsDropdownOpen(true);
+    };
+
+    const handleDropdownClose = () => {
+        setTimeout(() => {
+            setIsDropdownOpen(false);
+        }, 800);
+    };
+
+
+
     return (
-        <div>
-            <div className="allGamesMainDiv">
-                <div className='allGamesButtons'>
-                    <button className={earliestClassName} onClick={handleEarliestFirst}>Earliest First</button>
-                    <button className={latestClassName} onClick={handleLatestFirst}>Latest First</button>
-                </div>
-                <div className='allGamesScroller'>
-                    {gamesMapped}
+        <div className="allGamesMainDiv">
+            <div className="allGamesDropdown">
+                <div className="dropdownContainer" onMouseEnter={handleDropdownOpen} onMouseLeave={handleDropdownClose}>
+                    <FontAwesomeIcon icon={faFilter} className="filterIcon" />
+                    {isDropdownOpen && (
+                        <div className="allGamesDropdownContent">
+                            <button className={earliestClassName} onClick={handleEarliestFirst}>
+                                Earliest First
+                            </button>
+                            <button className={latestClassName} onClick={handleLatestFirst}>
+                                Latest First
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
+            <div className="allGamesScroller">{gamesMapped}</div>
         </div>
-    )
+    );
+
 }
 
 export default AllGames;
