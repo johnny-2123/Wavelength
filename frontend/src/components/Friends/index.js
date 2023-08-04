cd6b044bfb65b7e349fceb071a234db52b8363c4
edgar <johnnyavila2021@u.northwestern.edu>
2023-08-04 15:09:21 -0400

    fix container height responsiveness in friend details component

--- frontend/src/components/Friends/index.js
+++ frontend/src/components/Friends/index.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { NavLink, Route, Switch, useParams, useRouteMatch } from "react-router-dom";
+import {
+  NavLink,
+  Route,
+  Switch,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 
 import OpenModalButton from "../OpenModalButton";
 import AddFriend from "./AddFriendForm";
@@ -8,43 +14,57 @@ import PendingFriends from "./Pending Friends";
 import FriendDetails from "./Friend Details";
 import "./FriendsList.css";
 
-
 const FriendsList = ({ friends, sessionUser, sendMessage }) => {
-    const { path, url } = useRouteMatch();
-    const [activeTab, setActiveTab] = useState("accepted");
+  const { path, url } = useRouteMatch();
+  const [activeTab, setActiveTab] = useState("accepted");
 
-    const handleTabClick = (tab) => {
-        setActiveTab(tab);
-    };
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
 
-    return (
-        <div className="friendsList">
-            <nav className="friendsNavBar">
-                <div className='friendNavLinks'>
-                    <NavLink to={`${url}/accepted`} activeClassName="active-link" onClick={() => handleTabClick("accepted")}>
-                        Accepted
-                    </NavLink>
-                    <NavLink to={`${url}/pending`} activeClassName="active-link" onClick={() => handleTabClick("pending")}>
-                        Pending
-                    </NavLink>
-                </div>
-            </nav>
-            <div className="openFriendModalDiv">
-                <OpenModalButton modalComponent={<AddFriend />} buttonText={<i className="fa-solid fa-user-plus" />} />
-            </div>
-            <Switch>
-                <Route path={`${path}/accepted`}>
-                    <AcceptedFriends friends={friends} sessionUser={sessionUser} sendMessage={sendMessage} />
-                </Route>
-                <Route path={`${path}/pending`}>
-                    <PendingFriends friends={friends} sessionUser={sessionUser} />
-                </Route>
-                <Route path={`${path}/:friendId`}>
-                    <FriendDetails sessionUser={sessionUser} sendMessage={sendMessage} />
-                </Route>
-            </Switch>
+  return (
+    <div className="friendsList">
+      <nav className="friendsNavBar friendsNavBarOne">
+        <div className="friendNavLinks">
+          <NavLink
+            to={`${url}/accepted`}
+            activeClassName="active-link"
+            onClick={() => handleTabClick("accepted")}
+          >
+            Accepted
+          </NavLink>
+          <NavLink
+            to={`${url}/pending`}
+            activeClassName="active-link"
+            onClick={() => handleTabClick("pending")}
+          >
+            Pending
+          </NavLink>
         </div>
-    );
+        <div className="openFriendModalDiv">
+          <OpenModalButton
+            modalComponent={<AddFriend />}
+            buttonText={<i className="fa-solid fa-user-plus" />}
+          />
+        </div>
+      </nav>
+      <Switch>
+        <Route path={`${path}/accepted`}>
+          <AcceptedFriends
+            friends={friends}
+            sessionUser={sessionUser}
+            sendMessage={sendMessage}
+          />
+        </Route>
+        <Route path={`${path}/pending`}>
+          <PendingFriends friends={friends} sessionUser={sessionUser} />
+        </Route>
+        <Route path={`${path}/:friendId`}>
+          <FriendDetails sessionUser={sessionUser} sendMessage={sendMessage} />
+        </Route>
+      </Switch>
+    </div>
+  );
 };
 
 export default FriendsList;
