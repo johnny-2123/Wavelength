43cfdf3b66540013fd16d48382e301b3543fd086
edgar <johnnyavila2021@u.northwestern.edu>
2023-08-17 16:52:58 -0400

    add message to top friends component when no games have been played for the week

--- frontend/src/components/Home/LoggedInHomePage/LoggedInHomePageRootLanding/Top Friends/index.js
+++ frontend/src/components/Home/LoggedInHomePage/LoggedInHomePageRootLanding/Top Friends/index.js
@@ -2,25 +2,30 @@ import React from "react";
 import "./TopFriends.css";
 
 const TopFriends = ({ topFriends }) => {
-    const topFriendsMapped = topFriends?.map((friend) => {
-
-        return (
-            <div className="topFriendsItem">
-                <h3><i class="fa-solid fa-circle-dot"></i> {friend?.username}</h3>
-            </div>
-        )
-    })
-
+  console.log(topFriends);
+  const topFriendsMapped = topFriends?.map((friend) => {
     return (
-        <div className="topFriendsMainDiv">
-            <div className="topFriendsLeftDiv">
-                <h1>Week's Best Friends <i className="fa-solid fa-users"></i></h1>
-            </div>
-            <div className="topFriendsRightDiv">
-                {topFriendsMapped}
-            </div>
-        </div>
-    )
-}
+      <div className="topFriendsItem">
+        <h3>
+          <i class="fa-solid fa-circle-dot"></i> {friend?.username}
+        </h3>
+      </div>
+    );
+  });
+
+  return (
+    <div className="topFriendsMainDiv">
+      <div className="topFriendsLeftDiv">
+        <h1>
+          Week's Best Friends <i className="fa-solid fa-users"></i>
+        </h1>
+      </div>
+      <div className="topFriendsRightDiv">
+        {topFriends.length > 0 && topFriendsMapped}
+        {topFriends.length === 0 && <h4>No Games Played This Week</h4>}
+      </div>
+    </div>
+  );
+};
 
 export default TopFriends;
